4df3e5e72799fbfeacded4da337244dabbce8200
neocode <sheremet-oleksii@ukr.net>
2023-03-01 12:37:47 +0200

    First version.

---
+++ README.md
@@ -0,0 +1,20 @@
+# PubChem CID and SID Checker
+
+API for getting PubChem CID or SID by name.
+
+### Installation
+
+```
+pip install -r requirements.txt
+```
+
+### Run FastAPI application under Uvicorn
+
+```
+uvicorn main:app --reload
+```
+
+### API Usage Examples:
+
+![pubchem cid](pubchem_id_001.png)
+![pubchem sid](pubchem_id_002.png)
---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+import requests
+import urllib.parse as urllib_parse
+from fastapi import FastAPI
+
+app = FastAPI(title="PubChem CID and SID Checker", description="Getting PubChem CID or SID by name.", version="1.0")
+
+@app.get("/get_cid_by_name/{name}")
+async def get_cid_by_name(name: str):
+    try:
+        data = requests.get("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{}/JSON".\
+                            format(urllib_parse.quote(name))).json()
+        return str(data["PC_Compounds"][0]["id"]["id"]["cid"])
+    except:
+        return None
+    
+@app.get("/get_sid_by_name/{name}")
+async def get_sid_by_name(name: str):
+        try:
+            data = requests.get("https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/name/{}/sids/json".\
+                                format(urllib_parse.quote(name))).json()
+            sid = str(data["IdentifierList"]["SID"])
+            sid = sid.replace("[", "")
+            sid = sid.replace("]", "")
+            return sid
+        except:
+            return None
